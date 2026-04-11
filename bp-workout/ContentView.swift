e1f2221cc9baea41dd15b3ee81b8a3de5068632b
Neil Bhargava <neil.bhargava@beatport.com>
2026-04-11 11:32:27 -0700

    Initial Commit

---
+++ bp-workout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bp-workout
+//
+//  Created by Neil Bhargava on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bp-workout/bp_workoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  bp_workoutApp.swift
+//  bp-workout
+//
+//  Created by Neil Bhargava on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct bp_workoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
